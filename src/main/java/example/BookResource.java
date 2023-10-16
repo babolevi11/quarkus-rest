1d5dd649e843dfa5cb126aa71fdd336cde6216fa
levente.babocsai <levente.babocsai@cgi.com>
2023-10-16 15:38:05 +0200

    Change Book equals and hashcode

--- src/main/java/example/BookResource.java
+++ src/main/java/example/BookResource.java
@@ -10,18 +10,9 @@ import java.util.*;
 @Path("/books")
 public class BookResource {
 
-    //public List<Book> books = new ArrayList<>();
-    //private Set<Book> books = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
     public Map<Integer, Book> books = new HashMap<>();
 
     public BookResource() {
-        /*
-        this.books.add(new Book(1, "The Art of Programming", "John Smith", "023-01-15", 49.99F, "Computer Science", "9780123456789", 10));
-        this.books.add(new Book(2, "The Power of Words", "Emily Johnson", "2022-11-30", 29.99F, "Self-Help", "9789876543210", 5));
-        this.books.add(new Book(3, "A Journey Through Time", "David Thompson", "2023-03-22", 19.99F, "Fantasy", "9786543210987", 8));
-        this.books.add(new Book(4, "The Hidden Secrets", "Sarah Roberts", "2023-02-10", 14.99F, "Mystery", "9783210987654", 12));
-        this.books.add(new Book(5, "The Science of Nature", "Michael Anderson", "2023-04-18", 24.99F, "Science", "9785432109876", 3));
-         */
         this.books.put(1, new Book("The Art of Programming", "John Smith", "023-01-15", 49.99F, "Computer Science", "9780123456789", 10));
         this.books.put(2, new Book("The Power of Words", "Emily Johnson", "2022-11-30", 29.99F, "Self-Help", "9789876543210", 5));
         this.books.put(3, new Book("A Journey Through Time", "David Thompson", "2023-03-22", 19.99F, "Fantasy", "9786543210987", 8));
@@ -36,13 +27,7 @@ public class BookResource {
     }
 
     @POST
-    //public Map<Integer, Book> addBook(Book post) {
     public Response addBook(Book post) {
-        //if (!books.stream().filter(book -> book.getId() == post.getId()).findAny().isPresent()) {
-        //if (!books.stream().anyMatch(book -> book.getId() == post.getId())) {
-        //if (books.stream().noneMatch(book -> book.getId() == post.getId())) {
-        //    books.add(post);
-        //}
         if(!books.containsValue(post)) {
             Integer last = 0;
             for (Integer key : books.keySet()) {
@@ -58,13 +43,6 @@ public class BookResource {
 
     @PUT
     public Response updateBook(BookUpdate upd) {
-        //Optional<Book> old = books.stream().filter(book -> book.getId() == upd.getId()).findFirst();
-        /*
-        if (old.isPresent()) {
-            books.set(books.indexOf(old.get()), upd);
-        }
-         */
-        //old.ifPresent(book -> books.set(books.indexOf(book), upd));
         if (books.replace(upd.getId(), new Book(upd.getTitle(), upd.getAuthor(), upd.getPubDate(), upd.getPrice(), upd.getGenre(), upd.getIsbn(), upd.getQuantity())) != null) {
             return Response.ok(books).build();
         } else {
@@ -74,12 +52,6 @@ public class BookResource {
 
     @DELETE
     public Response deleteBook(BookUpdate del) {
-        /*
-        if (books.stream().filter(book -> book.getId() == del.getId()).findAny().isPresent()) {
-            books.remove(del);
-        }
-         */
-        //books.removeIf(book -> book.getId() == del.getId());
         if (books.remove(del.getId()) != null) {
             return Response.ok(books).build();
         } else {
--- src/main/java/example/pojos/Book.java
+++ src/main/java/example/pojos/Book.java
@@ -1,5 +1,7 @@
 package example.pojos;
 
+import java.util.Objects;
+
 public class Book {
     //private long id;
     private String title;
@@ -20,6 +22,18 @@ public class Book {
         this.genre = genre;
         this.isbn = isbn;
         this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(pubDate, book.pubDate) && Objects.equals(genre, book.genre) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, pubDate, genre, isbn);
     }
 
     //public long getId() { return id; }
